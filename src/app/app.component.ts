a4dc06a5dfd0cb99a0fc322bfc168c4e4a617de7
fcport <federico.casadei.portfolio@gmail.com>
2024-04-07 20:00:46 +0200

    feat: add tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,9 @@
+import {
+  TuiRootModule,
+  TuiDialogModule,
+  TuiAlertModule,
+  TuiButtonModule,
+} from '@taiga-ui/core';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
@@ -5,7 +11,14 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    TuiRootModule,
+    TuiDialogModule,
+    TuiAlertModule,
+    TuiButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
