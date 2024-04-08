bc47312528da2bec456fbda9222bba8a26ac16c1
fcport <federico.casadei.portfolio@gmail.com>
2024-04-08 11:47:02 +0200

    feat: scaffholding

---
+++ src/app/components/past-orders/past-orders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-past-orders',
+  standalone: true,
+  imports: [],
+  templateUrl: './past-orders.component.html',
+  styleUrl: './past-orders.component.scss'
+})
+export class PastOrdersComponent {
+
+}
