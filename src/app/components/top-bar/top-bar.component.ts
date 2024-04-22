eebf3f5b64557f28e0a7f902a3aa15714338ba32
fcport <federico.casadei.portfolio@gmail.com>
2024-04-22 09:12:21 +0200

    feat: add sidebar

--- src/app/components/top-bar/top-bar.component.ts
+++ src/app/components/top-bar/top-bar.component.ts
@@ -1,19 +1,28 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import { TuiSidebarModule } from '@taiga-ui/addon-mobile';
 import { TuiSvgModule } from '@taiga-ui/core';
 import { TuiBadgeModule } from '@taiga-ui/kit';
 import { CartService } from '../../services/cart.service';
+import { TuiActiveZoneModule } from '@taiga-ui/cdk';
 
 @Component({
   selector: 'app-top-bar',
   standalone: true,
-  imports: [RouterModule, TuiSvgModule, TuiBadgeModule],
+  imports: [
+    RouterModule,
+    TuiSvgModule,
+    TuiBadgeModule,
+    TuiSidebarModule,
+    TuiActiveZoneModule,
+  ],
   templateUrl: './top-bar.component.html',
   styleUrl: './top-bar.component.scss',
 })
 export class TopBarComponent {
   cartService = inject(CartService);
   numberInCart = this.cartService.totalItems;
+
   items = [
     {
       title: 'Home',
@@ -36,4 +45,10 @@ export class TopBarComponent {
       url: '/past-orders',
     },
   ];
+
+  openSidebar = false;
+
+  toggleSidebar() {
+    this.openSidebar = !this.openSidebar;
+  }
 }
